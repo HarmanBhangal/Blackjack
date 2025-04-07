379d3dc55f15eea665f1ca2cef2b1998aa42d019
HarmanBhangal <145411786+HarmanBhangal@users.noreply.github.com>
2025-04-07 00:53:08 -0400

    Delete client directory

--- client/src/constants/constants.ts
+++
@@ -1,6 +0,0 @@
-//base URl
-let parts = window.location.href.split(":");
-export const URL = parts[0] + ":" + parts[1] + ":3000";
-
-// polling constant (miliseconds)
-export const POLL_REFRESH_INTERVAL = 500;
